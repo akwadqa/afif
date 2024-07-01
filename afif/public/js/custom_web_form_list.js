7c4c56f68b19650051e18795980abaf58d422024
Santosh <alaa.hariri@outlook.com>
2024-07-01 13:26:08 +0300

    request button

--- afif/public/js/custom_web_form_list.js
+++ afif/public/js/custom_web_form_list.js
@@ -1,4 +1,31 @@
 frappe.ready(function() {
+    function checkRegistrationAndUpdateButton() {
+        if ($('body').data('path') === 'beneficiary-request3/list') {
+            frappe.call({
+                method: "frappe.client.get_list",
+                args: {
+                    doctype: "Beneficiaries Registration",
+                    fields: ["name"],
+                    filters: {
+                        status: "Accepted",
+                        user: frappe.session.user
+                    },
+                    limit_page_length: 1
+                },
+                callback: function(r) {
+                    console.log(r.message)
+                    if (r.message && r.message.length > 0) {
+                        $('.web-list-actions a.button-new').text('New Aid Request').show();
+                    } else {
+                        $('.web-list-actions a.button-new').hide();
+                    }
+                }
+            });
+        }
+    }
+
+    checkRegistrationAndUpdateButton();
+
     const observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             mutation.addedNodes.forEach(function(node) {
