bf215ea376b15becef3e7d6825a26e26aa640e61
Santosh <alaa.hariri@outlook.com>
2024-08-26 12:13:45 +0300

    updates

---
+++ afif/afif/doctype/sanadi_integration_settings/test_sanadi_integration_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Akwad and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSanadiIntegrationSettings(FrappeTestCase):
+	pass
