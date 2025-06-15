3ef705dcfa2268c9b5c0cfd984629ecd145b51d0
Alaa Hariri <alaa.hariri@outlook.com>
2025-06-15 16:27:36 +0300

    qatarpass logout override

--- afif/override/__init__.py
+++ afif/override/__init__.py
@@ -1,4 +1,4 @@
-import frappe
+from frappe import handler
 from afif.override.handler import web_logout as custom_web_logout
 
-frappe.handler.web_logout = custom_web_logout
+handler.web_logout = custom_web_logout
