2d8909712de268d9cdf5c236658319259ef39c73
Alaa Hariri <alaa.hariri@outlook.com>
2025-09-11 13:07:41 +0300

    Validate request_summary length (max 1000 characters) in Beneficiary Request Doctype.

--- afif/afif/doctype/beneficiary_request/beneficiary_request.py
+++ afif/afif/doctype/beneficiary_request/beneficiary_request.py
@@ -1,8 +1,11 @@
-# Copyright (c) 2023, Akwad and contributors
+# Copyright (c) 2025, Akwad and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class BeneficiaryRequest(Document):
-	pass
+    def validate(self):
+        if self.request_summary and len(self.request_summary) > 1000:
+            frappe.throw(_("Request Summary cannot be more than 1000 characters"))
