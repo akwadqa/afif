8114e02a366795b4ce84642037e9646c4e156f29
Neamah <neamah@akwad.qa>
2025-12-16 16:23:25 +0300

    Prevent approval when beneficiary ID is missing

--- afif/afif/doctype/beneficiary_request/beneficiary_request.js
+++ afif/afif/doctype/beneficiary_request/beneficiary_request.js
@@ -2,7 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Beneficiary Request', {
-	// refresh: function(frm) {
+	before_workflow_action(frm) {
+		if (frm.selected_workflow_action !== "Approve") return;
+		return new Promise((resolve, reject) => {
+			frappe.call({
+				method: "check_beneficiary_id",
+				doc: frm.doc,
+				callback(r) {
+					if (!r.message || r.message.success !== true) {
+						frappe.msgprint({
+							title: __('Cannot Approve'),
+							message: r.message?.message || __('Validation failed'),
+							indicator: 'red'
+						});
+						reject(); // stops workflow action
+						frappe.dom.unfreeze();
+						return;
+					}
 
-	// }
+					resolve();
+				},
+				error() {					
+					reject();
+				}
+			});
+		});
+	}
 });
--- afif/afif/doctype/beneficiary_request/beneficiary_request.py
+++ afif/afif/doctype/beneficiary_request/beneficiary_request.py
@@ -6,6 +6,29 @@ from frappe import _
 from frappe.model.document import Document
 
 class BeneficiaryRequest(Document):
+    @frappe.whitelist()
+    def check_beneficiary_id(self):
+
+        beneficiary = frappe.get_doc("Beneficiaries Registration", self.beneficiaries)
+        
+        if not beneficiary.ben_id:
+            frappe.log_error(f"beneficiary {beneficiary.name} didn\'t get id from Sanadi")
+            return {
+                "success": False,
+                "message": _("This beneficiary didn't get ID from Sanadi")
+            }   
+
+        return {
+            "success": True
+        }
+            
+
     def validate(self):
+        self.validate_request_summary()        
+
+    def validate_request_summary(self):
         if self.request_summary and len(self.request_summary) > 1000:
-            frappe.throw(_("Request Summary cannot be more than 1000 characters"))
+            frappe.throw(
+                _("Request Summary cannot be more than 1000 characters")
+            )
+
