18bd1f5cf95a796f44170de6eb08feef25f79b4f
Afnan <afnan@akwad.qa>
2026-07-07 13:55:59 +0300

    refactor: move apply_overrides function definition to the top of the file

--- afif/override/__init__.py
+++ afif/override/__init__.py
@@ -1,4 +1,4 @@
-from frappe import handler
-from afif.override.handler import web_logout as custom_web_logout
-
-handler.web_logout = custom_web_logout
+def apply_overrides(bootinfo=None):
+    from frappe import handler
+    from afif.override.handler import web_logout as custom_web_logout
+    handler.web_logout = custom_web_logout
