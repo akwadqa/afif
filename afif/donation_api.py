fcf0a0f38cd069f23d02cdfb73c30e1e618b9dc9
Afnan <afnan@akwad.qa>
2026-07-08 18:03:25 +0300

    feat: implement initial donation APIs

---
+++ afif/donation_api.py
@@ -0,0 +1,80 @@
+# Copyright (c) 2026, Akwad and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+
+
+@frappe.whitelist(allow_guest=True)
+def get_programs():
+	return frappe.get_all(
+		"Donation Program",
+		filters={"published": 1},
+		fields=["name", "title", "intro", "display_order"],
+		order_by="display_order asc, title asc",
+		ignore_permissions=True,
+	)
+
+
+@frappe.whitelist(allow_guest=True)
+def get_projects(program):
+	if not frappe.db.exists("Donation Program", {"name": program, "published": 1}):
+		frappe.throw(_("Donation Program not found"), frappe.DoesNotExistError)
+
+	return frappe.get_all(
+		"Donation Project",
+		filters={"program": program, "published": 1},
+		fields=["name", "title", "quote", "body", "min_amount", "display_order"],
+		order_by="display_order asc, title asc",
+		ignore_permissions=True,
+	)
+
+
+@frappe.whitelist(allow_guest=True)
+@frappe.rate_limit(limit=10, seconds=60 * 60)
+def create_donation(donation_project, amount, donor_name, donor_mobile, donor_email=None):
+	project = frappe.db.get_value(
+		"Donation Project",
+		{"name": donation_project, "published": 1},
+		["name", "min_amount"],
+		as_dict=True,
+	)
+	if not project:
+		frappe.throw(_("Donation Project not found"), frappe.DoesNotExistError)
+
+	amount = frappe.utils.flt(amount)
+	if amount < frappe.utils.flt(project.min_amount):
+		frappe.throw(_("Amount must be at least {0}").format(project.min_amount))
+
+	donation = frappe.get_doc({
+		"doctype": "Donation",
+		"donation_project": project.name,
+		"amount": amount,
+		"donor_name": donor_name,
+		"donor_mobile": donor_mobile,
+		"donor_email": donor_email,
+	})
+	donation.insert(ignore_permissions=True)
+	frappe.db.commit()
+
+	# Dibsy checkout session creation is wired in here once the gateway integration is added.
+	return {
+		"reference_id": donation.reference_id,
+		"amount": donation.amount,
+		"currency": donation.currency,
+	}
+
+
+@frappe.whitelist(allow_guest=True)
+@frappe.rate_limit(limit=30, seconds=60 * 60)
+def get_status(reference_id):
+	donation = frappe.db.get_value(
+		"Donation",
+		{"reference_id": reference_id},
+		["payment_status", "amount", "currency", "donor_name"],
+		as_dict=True,
+	)
+	if not donation:
+		frappe.throw(_("Donation not found"), frappe.DoesNotExistError)
+
+	return donation
